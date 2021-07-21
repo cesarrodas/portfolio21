59e658ef8d7b2cb5cdc2c87854cf6904e3701d40
cesar rodas <rodascesar03@yahoo.com>
2021-07-21 15:41:17 -0500

    updated structure

--- public/main.js
+++ public/main.js
@@ -1,1 +1,8 @@
-console.log("yo!")
+const home = document.getElementById("home")
+const about = document.getElementById("about")
+const projects = document.getElementById("projects")
+const contact = document.getElementById("contact")
+
+about.style.display = "none"
+projects.style.display = "none"
+contact.style.display = "none"
