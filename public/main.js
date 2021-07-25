83992c521b7bf5790ff1dc2a1223ae9a01732398
Cesar <rodascesar03@yahoo.com>
2021-07-24 23:31:45 -0500

    updating home page

--- public/main.js
+++ public/main.js
@@ -2,7 +2,12 @@ const home = document.getElementById("home")
 const about = document.getElementById("about")
 const projects = document.getElementById("projects")
 const contact = document.getElementById("contact")
+const loading = document.getElementById("loading")
+const menu = document.getElementById("menu")
 
+loading.style.display = "none"
+menu.style.display = "flex"
+home.style.display = "block"
 about.style.display = "none"
 projects.style.display = "none"
 contact.style.display = "none"
