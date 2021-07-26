2c02944a329f7aded9a8af06e42aba8c319d5162
cesar rodas <rodascesar03@yahoo.com>
2021-07-25 19:26:11 -0500

    adding to page backgrounds

--- public/main.js
+++ public/main.js
@@ -3,11 +3,11 @@ const about = document.getElementById("about")
 const projects = document.getElementById("projects")
 const contact = document.getElementById("contact")
 const loading = document.getElementById("loading")
-const menu = document.getElementById("menu")
+const menu = document.getElementsByClassName("menu")[0];
 
 loading.style.display = "none"
 menu.style.display = "flex"
-home.style.display = "block"
-about.style.display = "none"
-projects.style.display = "none"
-contact.style.display = "none"
+//home.style.display = "block"
+//about.style.display = "block"
+//projects.style.display = "block"
+contact.style.display = "block"
